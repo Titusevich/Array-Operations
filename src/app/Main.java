692bf9f7ea21e22b14fb5c2660f09e46b4b2a194
Titusevich <nickitatitaevskiy@gmail.com>
2024-12-20 11:22:28 +0200

    Add array info

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.util.Arrays;
+
 public class Main {
     static double[] array;
     static double sumNegativeValues;
@@ -15,5 +17,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        array = new double[]{58, -74, 23, 66, -11, -22, 36, 25, 88, 4,
+                19, -18, -27, 56, -7, 16, 47, -85, 91, 34};
+
+        int arrLength = array.length;
+        System.out.println("\n1) Elements of array: " + Arrays.toString(array));
+        System.out.println("2) Array length is " + arrLength);
     }
 }
