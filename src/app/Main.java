fd4bce3869e7b7e398840816f19569ccf3fdbd16
Titusevich <nickitatitaevskiy@gmail.com>
2024-12-20 11:13:42 +0200

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,19 @@
+package app;
+
+public class Main {
+    static double[] array;
+    static double sumNegativeValues;
+    static int countEvenNumbers;
+    static int countOddNumbers;
+    static double minElement;
+    static double maxElement;
+    static int maxIndex;
+    static int minIndex;
+    static boolean findNegativeValues;
+    static double summa;
+    static double countElements;
+
+    public static void main(String[] args) {
+
+    }
+}
